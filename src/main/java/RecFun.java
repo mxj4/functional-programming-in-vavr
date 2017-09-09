5629b593521f86a7f813661029eb4a3f3bfadd17
Yue Liu <yue.liu@mail.com>
2017-09-09 12:14:50 -0700

    Add recfun

---
+++ src/main/java/RecFun.java
@@ -0,0 +1,31 @@
+import io.vavr.collection.*;
+
+// Recursion
+public class RecFun {
+    // ex1
+    public static Integer pascal(Integer c, Integer r) {
+        if (r == 0 || c == 0 || c == r) return 1;
+        return pascal(c-1, r-1) + pascal(c, r-1);
+    }
+
+    // ex2
+    public static Boolean balance(CharSeq chars) {
+        return balanceRec(chars, 0) == 0;
+    }
+
+    private static Integer balanceRec(CharSeq chars, Integer acc) {
+        if (chars.isEmpty() || acc < 0) return acc;
+        if (chars.head() == '(') return balanceRec(chars.tail(), acc + 1);
+        if (chars.head() == ')') return balanceRec(chars.tail(), acc -1);
+        return balanceRec(chars.tail(), acc);
+    }
+
+    // ex3
+    public static Integer countChange(Integer money, Seq<Integer> coins) {
+        if (money == 0) return 1;
+        if (coins.isEmpty()) return 0;
+        if (money < 0) return 0;
+        return countChange(money, coins.tail()) +
+                countChange(money - coins.head(), coins);
+    }
+}
---
+++ src/test/java/RecFunTest.java
@@ -0,0 +1,39 @@
+import io.vavr.collection.CharSeq;
+import io.vavr.collection.List;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RecFunTest {
+    @Test
+    public void testPascal() {
+        assertEquals(Integer.valueOf(1), RecFun.pascal(0, 2));
+        assertEquals(Integer.valueOf(2), RecFun.pascal(1, 2));
+        assertEquals(Integer.valueOf(3), RecFun.pascal(1, 3));
+    }
+
+    @Test
+    public void testBalance() {
+        assertTrue(RecFun.balance(CharSeq.of("(if (zero? x) max (/ 1 x))")));
+        assertTrue(RecFun.balance(CharSeq.of("I told him (that it's not (yet) done).\n(But he wasn't listening)")));
+        assertFalse(RecFun.balance(CharSeq.of(":-)")));
+        assertFalse(RecFun.balance(CharSeq.of("())(")));
+
+        assertTrue(RecFun.balance(CharSeq.of("")));
+        assertFalse(RecFun.balance(CharSeq.of("(")));
+        assertFalse(RecFun.balance(CharSeq.of(")")));
+        assertTrue(RecFun.balance(CharSeq.of("()")));
+        assertFalse(RecFun.balance(CharSeq.of(")(")));
+        assertFalse(RecFun.balance(CharSeq.of(")()(")));
+    }
+
+    @Test
+    public void testConutntChange() {
+        assertEquals(Integer.valueOf(3), RecFun.countChange(4, List.of(1, 2)));
+        assertEquals(Integer.valueOf(1022), RecFun.countChange(300, List.of(5,10,20,50,100,200,500)));
+        assertEquals(Integer.valueOf(0), RecFun.countChange(301, List.of(5,10,20,50,100,200,500)));
+        assertEquals(Integer.valueOf(1022), RecFun.countChange(300, List.of(500,5,50,100,20,200,10)));
+    }
+}
